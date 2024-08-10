154100b9214eadab648cbf8ca580fc178c6ba37d
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-08-10 12:23:29 +0200

    second commit

---
+++ src/components/HelloWorld copy 4.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="hello">
+   
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Jumbutron.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7"><img src="" alt=""></div>
+      <div class="col-md-5">Text</div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
